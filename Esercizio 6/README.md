4d6cd26c40a974b5de31522b2b458eae74766b62
Chiara <chiarafioref@gmail.com>
2026-06-26 17:14:32 +0200

    implemented basic arithmetic operations exercise with JS variables

---
+++ Esercizio 6/README.md
@@ -0,0 +1,3 @@
+# ESERCIZIO
+
+Usando 20 e 4, calcola somma, prodotto, divisione, differenza e resto.
---
+++ Esercizio 6/main.js
@@ -0,0 +1,19 @@
+console.log("script JS caricato");
+
+const a = 20;
+const b = 4;
+
+const sum = a + b;
+console.log(sum);
+
+const product = a * b;
+console.log(product);
+
+const division = a / b;
+console.log(division);
+
+const difference = a - b;
+console.log(difference);
+
+const rest = a % b;
+console.log(rest);
